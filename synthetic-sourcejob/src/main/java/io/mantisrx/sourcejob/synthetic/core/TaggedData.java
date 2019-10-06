4627f218fc14ad2ebf572098400199a433e4405e
Neeraj Joshi <njoshi@netflix.com>
2019-10-06 14:07:33 -0700

    Add missing license headers

--- synthetic-sourcejob/src/main/java/io/mantisrx/sourcejob/synthetic/core/TaggedData.java
+++ synthetic-sourcejob/src/main/java/io/mantisrx/sourcejob/synthetic/core/TaggedData.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.mantisrx.sourcejob.synthetic.core;
 
 import java.util.HashMap;
@@ -10,7 +26,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.mantisrx.common.codec.Codec;
 import io.mantisrx.runtime.codec.JsonType;
-
 
 public class TaggedData implements JsonType {
 
@@ -26,7 +41,6 @@ public class TaggedData implements JsonType {
     public Set<String> getMatchedClients() {
         return matchedClients;
     }
-
 
     public boolean matchesClient(String clientId) {
         return matchedClients.contains(clientId);
